ee6d4a28ce0d0d4e7d4e589467d3423902afd623
Sultan <138652076+sulfar0@users.noreply.github.com>
2026-01-31 17:19:07 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -83,7 +83,7 @@ function user {
 
 # grub
 function grub_install {
-    arch-chroot /mnt grub-install --target=i386-pc /dev/sda1 &&
+    arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Arch 
 }
 
 
